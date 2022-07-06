a0945c82dd9a0ab5e6e742fc28b13d05781bbbd8
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-06 14:30:19 +0200

    Implemented Get function

---
+++ pkg/controllers/user.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"database/sql"
+	db "github.com/BoggerByte/Sentinel-backend.git/pkg/db/sqlc"
+	"github.com/BoggerByte/Sentinel-backend.git/pkg/middlewares"
+	"github.com/BoggerByte/Sentinel-backend.git/pkg/modules/token"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type UserController struct {
+	store db.Store
+}
+
+func NewUserController(store db.Store) *UserController {
+	return &UserController{store: store}
+}
+
+func (ctrl *UserController) Get(c *gin.Context) {
+	payload := c.MustGet(middlewares.AuthorizationPayloadKey).(*token.Payload)
+
+	account, err := ctrl.store.GetUser(c, payload.UserDiscordID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.AbortWithStatusJSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		c.AbortWithStatusJSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	c.JSON(http.StatusOK, account)
+}

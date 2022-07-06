1c02df07117ebb2a2bc191bfadf3f92debb2b8df
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-06 14:19:57 +0200

    Implemented guild config structure and default guild config object

---
+++ pub/objects/guild_config.go
@@ -0,0 +1,25 @@
+package objects
+
+type GuildConfigPermissions struct {
+	Edit int64 `json:"edit"`
+	Read int64 `json:"read"`
+}
+
+type GuildConfigData struct {
+	UseConfig bool `json:"use_config"`
+}
+
+type GuildConfig struct {
+	Permissions GuildConfigPermissions `json:"permissions"`
+	Data        GuildConfigData        `json:"data"`
+}
+
+var DefaultGuildConfig = GuildConfig{
+	Permissions: GuildConfigPermissions{
+		Edit: 40,
+		Read: 0xfffffffffff, // @everyone permissions
+	},
+	Data: GuildConfigData{
+		UseConfig: false,
+	},
+}

21ef5a3e6826140cedaf1df1b5b131711897a57b
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-06 14:07:06 +0200

    Implemented authentication & authorization middleware

---
+++ pkg/middlewares/auth.go
@@ -0,0 +1,51 @@
+package middlewares
+
+import (
+	"errors"
+	"fmt"
+	"github.com/BoggerByte/Sentinel-backend.git/pkg/modules/token"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+const (
+	AuthorizationHeaderKey  = "authorization"
+	AuthorizationTypeBearer = "bearer"
+	AuthorizationPayloadKey = "authorization_payload"
+)
+
+func NewAuthMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader(AuthorizationHeaderKey)
+		if len(authHeader) == 0 {
+			err := errors.New("authentication header is not provided")
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+			return
+		}
+
+		fields := strings.Fields(authHeader)
+		if len(fields) != 2 {
+			err := errors.New("invalid authentication header format")
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+			return
+		}
+
+		authType := strings.ToLower(fields[0])
+		if authType != AuthorizationTypeBearer {
+			err := fmt.Errorf("unsupported authentication type: %s", authType)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+			return
+		}
+
+		accessToken := fields[1]
+		payload, err := tokenMaker.VerifyToken(accessToken)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+			return
+		}
+
+		c.Set(AuthorizationPayloadKey, payload)
+		c.Next()
+	}
+}

ab7d67cb85d3f10212c440fa4e615894caefcfec
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-06 14:28:46 +0200

    Dependency injection like refactoring

--- pkg/server.go
+++ pkg/server.go
@@ -3,80 +3,49 @@ package pkg
 import (
 	"github.com/BoggerByte/Sentinel-backend.git/pkg/controllers"
 	"github.com/BoggerByte/Sentinel-backend.git/pkg/middlewares"
-	"github.com/BoggerByte/Sentinel-backend.git/pkg/services"
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
-	"github.com/ravener/discord-oauth2"
-	"golang.org/x/oauth2"
 	"net/http"
-	"os"
 )
 
 type Server struct {
 	router *gin.Engine
 }
 
-func NewServer() *Server {
+func NewServer(controllers controllers.Controllers, middlewares middlewares.Middlewares) *Server {
 	router := gin.Default()
 
-	router.LoadHTMLGlob("./public/html/*")
-	router.Static("/public", "./public")
+	router.LoadHTMLGlob("./pub/html/*")
+	router.Static("/pub", "./pub")
 
-	router.Use(middlewares.CORSMiddleware())
-	router.Use(middlewares.RequestIDMiddleware())
-	router.Use(gzip.Gzip(gzip.DefaultCompression))
+	perms := middlewares.Permissions
 
-	api := router.Group("/api/v1")
-	{
-		/*** START OAUTH2 ***/
-		discordOauth2Service := services.NewDiscordOauth2Service(&oauth2.Config{
-			Endpoint:     discord.Endpoint,
-			Scopes:       []string{discord.ScopeIdentify, discord.ScopeEmail, discord.ScopeGuilds},
-			RedirectURL:  "http://localhost:8080/api/v1/oauth2/redirect",
-			ClientID:     os.Getenv("DISCORD_CLIENT_ID"),
-			ClientSecret: os.Getenv("DISCORD_CLIENT_SECRET"),
-		})
-		oauth := controllers.NewOauth2Controller(discordOauth2Service)
-
-		api.GET("/oauth2/generate_url", oauth.GenerateURL)
-		api.GET("/oauth2/redirect", oauth.HandleRedirect)
-
-		/*** START AUTH ***/
-		auth := new(controllers.AuthController)
-
-		api.POST("/auth/jwt/login", auth.AuthorizeToken)
-		api.POST("/auth/jwt/refresh", auth.RefreshToken)
-
-		/*** START ADMINS ***/
-		admin := controllers.NewAdminController()
-
-		api.POST("/admins", admin.Create)
-		api.GET("/admins/:id", admin.Get)
-
-		/*** START GUILDS ***/
-		guild := new(controllers.GuildController)
-
-		api.GET("/guilds/:id", guild.Get)
-
-		/*** START CONFIGS ***/
-		config := new(controllers.ConfigController)
-
-		api.GET("/guilds/:id/configs", config.GetAll)
-		api.POST("/guilds/:id/configs/:v", config.Create)
-		api.GET("/guilds/:id/configs/:v", config.Get)
-		api.PUT("/guilds/:id/configs/:v", config.Update)
-		api.DELETE("/guilds/:id/configs/:v", config.Delete)
-	}
+	/* INITIALIZING ROUTES */
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Root",
-		})
+		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 
 	router.NoRoute(func(c *gin.Context) {
-		c.HTML(404, "404.html", gin.H{})
+		c.HTML(http.StatusNotFound, "404.html", gin.H{})
 	})
+
+	api := router.Group("/api/v1").
+		Use(gzip.Gzip(gzip.DefaultCompression))
+	{
+		api.GET("/oauth2/generate_url", controllers.Oauth2.GenerateURL)
+		api.GET("/oauth2/redirect", controllers.Oauth2.HandleRedirect)
+
+		api.POST("/auth/paseto/refresh", middlewares.Auth, controllers.Auth.RefreshToken)
+
+		api.GET("/users/me", middlewares.Auth, controllers.Account.Get)
+		api.GET("/users/me/guilds", middlewares.Auth, controllers.Guild.GetAll)
+
+		api.GET("/guilds/:discord_id", controllers.Guild.Get)
+
+		api.POST("/guilds/:discord_id/config", middlewares.Auth, perms.GuildConfig.Overwrite(), controllers.GuildConfig.Overwrite)
+		api.GET("/guilds/:discord_id/config", middlewares.Auth, perms.GuildConfig.Get(), controllers.GuildConfig.Get)
+	}
 
 	return &Server{router: router}
 }

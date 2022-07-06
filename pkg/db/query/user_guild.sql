0f8acc785b4579785a076bbe958d5128d54e870c
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-06 14:30:53 +0200

    Implemented CreateOrUpdateUserGuildRel query

---
+++ pkg/db/query/user_guild.sql
@@ -0,0 +1,24 @@
+-- name: CreateUserGuildRel :one
+INSERT INTO user_guild (guild_discord_id, account_discord_id, permissions)
+VALUES ($1, $2, $3)
+RETURNING *;
+
+-- name: GetUserGuildRel :one
+SELECT *
+FROM user_guild
+WHERE account_discord_id = $1
+  AND guild_discord_id = $2
+LIMIT 1;
+
+-- name: CreateOrUpdateUserGuildRel :one
+INSERT INTO user_guild (guild_discord_id, account_discord_id, permissions)
+VALUES ($1, $2, $3)
+ON CONFLICT (guild_discord_id, account_discord_id) DO UPDATE
+    SET permissions = $3
+RETURNING *;
+
+-- name: DeleteUserGuildRel :exec
+DELETE
+FROM user_guild
+WHERE account_discord_id = $1
+  AND guild_discord_id = $2;

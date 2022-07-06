6e23018d74b7d0ac7a30eeac2dadb5bcb43e2c0e
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-06 14:30:39 +0200

    Implemented CreateOrUpdateUser query

---
+++ pkg/db/query/user.sql
@@ -0,0 +1,24 @@
+-- name: CreateOrUpdateUser :one
+INSERT INTO "user" (discord_id, username, discriminator, verified, email,
+                    avatar, banner, accent_color)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+ON CONFLICT (discord_id) DO UPDATE
+    SET username      = $2,
+        discriminator = $3,
+        verified      = $4,
+        email         = $5,
+        avatar        = $6,
+        banner        = $8,
+        accent_color  = $9
+RETURNING *;
+
+-- name: GetUser :one
+SELECT *
+FROM "user"
+WHERE discord_id = $1
+LIMIT 1;
+
+-- name: DeleteUser :exec
+DELETE
+FROM "user"
+WHERE discord_id = $1;

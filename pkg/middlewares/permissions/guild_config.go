993cfdd5d8dd1224102b297cbc9e442ff6e509e0
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-06 14:19:24 +0200

    Implemented permissions for guild/:discord_id/config routes

---
+++ pkg/middlewares/permissions/guild_config.go
@@ -0,0 +1,130 @@
+package permissions
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	db "github.com/BoggerByte/Sentinel-backend.git/pkg/db/sqlc"
+	"github.com/BoggerByte/Sentinel-backend.git/pkg/forms"
+	"github.com/BoggerByte/Sentinel-backend.git/pkg/middlewares"
+	"github.com/BoggerByte/Sentinel-backend.git/pkg/modules/token"
+	"github.com/BoggerByte/Sentinel-backend.git/pub/objects"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func anyOfPermissions(p1 int64, p2 int64) bool {
+	return p1&p2 > 0
+}
+
+type GuildConfigPermissions struct {
+	store db.Store
+}
+
+func NewGuildConfigPermissions(store db.Store) *GuildConfigPermissions {
+	return &GuildConfigPermissions{
+		store: store,
+	}
+}
+
+func (p *GuildConfigPermissions) Overwrite() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var req forms.RequireDiscordIDRequest
+		if err := c.ShouldBindUri(&req); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
+
+		authPayload := c.MustGet(middlewares.AuthorizationPayloadKey).(*token.Payload)
+
+		userGuildRel, err := p.store.GetUserGuildRel(c, db.GetUserGuildRelParams{
+			AccountDiscordID: authPayload.UserDiscordID,
+			GuildDiscordID:   req.DiscordID,
+		})
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				err := errors.New("no relations with guild")
+				c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"message": err.Error()})
+				return
+			}
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
+		}
+
+		guildConfig, err := p.store.GetGuildConfig(c, req.DiscordID)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				err := errors.New("guild config not found")
+				c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"message": err.Error()})
+				return
+			}
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
+		}
+
+		var guildConfigObj = objects.DefaultGuildConfig
+		err = json.Unmarshal(guildConfig.Json, &guildConfigObj)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
+		}
+
+		if !anyOfPermissions(userGuildRel.Permissions, guildConfigObj.Permissions.Edit) {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func (p *GuildConfigPermissions) Get() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var req forms.RequireDiscordIDRequest
+		if err := c.ShouldBindUri(&req); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
+
+		authPayload := c.MustGet("authorization_payload").(*token.Payload)
+
+		userGuildRel, err := p.store.GetUserGuildRel(c, db.GetUserGuildRelParams{
+			AccountDiscordID: authPayload.UserDiscordID,
+			GuildDiscordID:   req.DiscordID,
+		})
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				err := errors.New("no relations with guild")
+				c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"message": err.Error()})
+				return
+			}
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
+		}
+
+		guildConfig, err := p.store.GetGuildConfig(c, req.DiscordID)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				err := errors.New("guild config not found")
+				c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"message": err.Error()})
+				return
+			}
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
+		}
+
+		var guildConfigObj = objects.DefaultGuildConfig
+		err = json.Unmarshal(guildConfig.Json, &guildConfigObj)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
+		}
+
+		if !anyOfPermissions(userGuildRel.Permissions, guildConfigObj.Permissions.Read) {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
+		}
+
+		c.Next()
+	}
+}

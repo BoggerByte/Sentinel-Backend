e3e6b63977864dcaaabdc4e67fddcd39cc594cb0
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-06 14:17:33 +0200

    Implemented GetUserGuilds and CreateOrUpdateGuild queries

---
+++ pkg/db/query/guild.sql
@@ -0,0 +1,30 @@
+-- name: CreateOrUpdateGuild :one
+INSERT INTO guild (discord_id, name, icon, owner_discord_id)
+VALUES ($1, $2, $3, $4)
+ON CONFLICT (discord_id) DO UPDATE
+    SET name=$2,
+        icon=$3,
+        owner_discord_id=$4
+RETURNING *;
+
+-- name: GetGuild :one
+SELECT *
+FROM guild
+WHERE discord_id = $1
+LIMIT 1;
+
+-- name: DeleteGuild :exec
+DELETE
+FROM guild
+WHERE discord_id = $1;
+
+-- name: GetUserGuilds :many
+SELECT coalesce(g.id, 0),
+       ug.guild_discord_id AS discord_id,
+       ug.permissions,
+       coalesce(g.owner_discord_id, 0),
+       coalesce(g.icon, '#'),
+       coalesce(g.name, '')
+FROM user_guild ug
+         LEFT OUTER JOIN guild g ON g.discord_id = ug.guild_discord_id
+WHERE ug.account_discord_id = $1;

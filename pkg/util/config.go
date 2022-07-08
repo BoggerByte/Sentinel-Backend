0427a7cf50694d10df1cc325a7a242430056b471
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-08 15:06:13 +0200

    New fields for redis DB

--- pkg/util/config.go
+++ pkg/util/config.go
@@ -6,20 +6,24 @@ import (
 )
 
 type Config struct {
-	Address              string        `mapstructure:"address"`
-	DBDriver             string        `mapstructure:"db-driver"`
-	DBProtocol           string        `mapstructure:"db-protocol"`
-	DBHost               string        `mapstructure:"db-host"`
-	DBPort               string        `mapstructure:"db-port"`
-	DBUsername           string        `mapstructure:"DB_USERNAME"`
-	DBPassword           string        `mapstructure:"DB_PASSWORD"`
-	DBName               string        `mapstructure:"db-name"`
-	DBSSLMode            string        `mapstructure:"db-ssl-mode"`
-	PasetoSymmetricKey   string        `mapstructure:"PASETO_SYMMETRIC_KEY"`
-	AccessTokenDuration  time.Duration `mapstructure:"token-access-duration"`
-	RefreshTokenDuration time.Duration `mapstructure:"token-refresh-duration"`
-	DiscordClientID      string        `mapstructure:"DISCORD_CLIENT_ID"`
-	DiscordClientSecret  string        `mapstructure:"DISCORD_CLIENT_SECRET"`
+	Address                 string        `mapstructure:"address"`
+	DBDriver                string        `mapstructure:"db-driver"`
+	DBProtocol              string        `mapstructure:"db-protocol"`
+	DBHost                  string        `mapstructure:"db-host"`
+	DBPort                  string        `mapstructure:"db-port"`
+	DBUsername              string        `mapstructure:"DB_USERNAME"`
+	DBPassword              string        `mapstructure:"DB_PASSWORD"`
+	DBName                  string        `mapstructure:"db-name"`
+	DBSSLMode               string        `mapstructure:"db-ssl-mode"`
+	RedisHost               string        `mapstructure:"redis-host"`
+	RedisPort               string        `mapstructure:"redis-port"`
+	RedisPassword           string        `mapstructure:"REDIS_PASSWORD"`
+	PasetoSymmetricKey      string        `mapstructure:"PASETO_SYMMETRIC_KEY"`
+	AccessTokenDuration     time.Duration `mapstructure:"token-access-duration"`
+	RefreshTokenDuration    time.Duration `mapstructure:"token-refresh-duration"`
+	Oauth2FlowStateDuration time.Duration `mapstructure:"oauth2flow-state-duration"`
+	DiscordClientID         string        `mapstructure:"DISCORD_CLIENT_ID"`
+	DiscordClientSecret     string        `mapstructure:"DISCORD_CLIENT_SECRET"`
 }
 
 func LoadConfig() (Config, error) {

3b7577b6b70992bba0dc7608dccfda065b72ea10
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-08 15:10:26 +0200

    Implemented memory store (redis)

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/BoggerByte/Sentinel-backend.git/pkg"
 	"github.com/BoggerByte/Sentinel-backend.git/pkg/controllers"
+	memdb "github.com/BoggerByte/Sentinel-backend.git/pkg/db/memory"
 	"github.com/BoggerByte/Sentinel-backend.git/pkg/db/sqlc"
 	"github.com/BoggerByte/Sentinel-backend.git/pkg/middlewares"
 	"github.com/BoggerByte/Sentinel-backend.git/pkg/middlewares/permissions"
 	"github.com/BoggerByte/Sentinel-backend.git/pkg/modules/token"
 	"github.com/BoggerByte/Sentinel-backend.git/pkg/services"
 	"github.com/BoggerByte/Sentinel-backend.git/pkg/util"
+	"github.com/gin-contrib/cors"
 	_ "github.com/lib/pq"
 	"github.com/ravener/discord-oauth2"
 	"github.com/sirupsen/logrus"
@@ -21,7 +23,7 @@ func main() {
 		logrus.Fatalf("Failed to initialize config: %v", err.Error())
 	}
 
-	store, err := db.Init(db.ConnectionConfig{
+	store, err := db.NewSQLStore(db.ConnectionConfig{
 		Driver:   config.DBDriver,
 		Protocol: config.DBProtocol,
 		Username: config.DBUsername,
@@ -35,6 +37,16 @@ func main() {
 		logrus.Fatalf("Failed to connect to DB: %v", err.Error())
 	}
 
+	memStore, err := memdb.NewRedisStore(memdb.ConnectionConfig{
+		Host:     config.RedisHost,
+		Port:     config.RedisPort,
+		Password: config.RedisPassword,
+		DB:       0, // default
+	})
+	if err != nil {
+		logrus.Fatalf("Failed to connect to Memomry DB: %s", err.Error())
+	}
+
 	tokenMaker, err := token.NewPasetoMaker(config.PasetoSymmetricKey)
 	if err != nil {
 		logrus.Fatalf("Failed to create PASeTo token maker: %v", err.Error())
@@ -43,20 +55,21 @@ func main() {
 	discordOauth2Service := services.NewDiscordOauth2Service(&oauth2.Config{
 		Endpoint:     discord.Endpoint,
 		Scopes:       []string{discord.ScopeIdentify, discord.ScopeEmail, discord.ScopeGuilds},
-		RedirectURL:  "http://localhost:8080/api/v1/oauth2/redirect",
+		RedirectURL:  "http://localhost:8080/api/v1/oauth2/discord_callback",
 		ClientID:     config.DiscordClientID,
 		ClientSecret: config.DiscordClientSecret,
 	})
 
 	controllersV1 := controllers.Controllers{
 		Account:     controllers.NewUserController(store),
-		Auth:        controllers.NewAuthController(store, config, tokenMaker),
+		Auth:        controllers.NewAuthController(store, memStore, config, tokenMaker),
 		Guild:       controllers.NewGuildController(store),
 		GuildConfig: controllers.NewGuildConfigController(store),
-		Oauth2:      controllers.NewOauth2Controller(store, config, tokenMaker, discordOauth2Service),
+		Oauth2:      controllers.NewOauth2Controller(store, memStore, config, tokenMaker, discordOauth2Service),
 	}
 
 	middlewaresV1 := middlewares.Middlewares{
+		CORS: cors.Default(),
 		Auth: middlewares.NewAuthMiddleware(tokenMaker),
 		Permissions: middlewares.Permissions{
 			GuildConfig: permissions.NewGuildConfigPermissions(store),

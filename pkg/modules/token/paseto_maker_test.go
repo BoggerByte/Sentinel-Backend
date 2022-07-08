99164999fc4512b4716648d466271978f20eb1d1
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-08 15:16:30 +0200

    Updated tests for paseto_maker.go

--- pkg/modules/token/paseto_maker_test.go
+++ pkg/modules/token/paseto_maker_test.go
@@ -39,7 +39,7 @@ func TestNewPasetoMaker(t *testing.T) {
 }
 
 func TestPasetoTokenMaker(t *testing.T) {
-	accountDiscordID := util.GenerateSnowflakeID().Int64()
+	userDiscordID := util.RandomSnowflakeID().Int64()
 	issuedAt := time.Now()
 
 	testCases := []struct {
@@ -47,53 +47,27 @@ func TestPasetoTokenMaker(t *testing.T) {
 		scope         string
 		userDiscordID int64
 		duration      time.Duration
-		createToken   func(t *testing.T, scope string, userDiscordID int64, duration time.Duration)
 		checkVerify   func(t *testing.T, payload *Payload, err error)
 	}{
 		{
 			name:          "OK",
-			scope:         ScopeAccess,
-			userDiscordID: util.GenerateSnowflakeID().Int64(),
+			userDiscordID: userDiscordID,
 			duration:      time.Minute,
-			createToken: func(t *testing.T, scope string, userDiscordID int64, duration time.Duration) {
-
-			},
 			checkVerify: func(t *testing.T, payload *Payload, err error) {
 				require.NoError(t, err)
 				require.NotEmpty(t, payload)
 				require.NotZero(t, payload.ID)
-				require.Equal(t, accountDiscordID, payload.UserDiscordID)
+				require.Equal(t, userDiscordID, payload.UserDiscordID)
 				require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
 				require.WithinDuration(t, issuedAt.Add(time.Minute), payload.ExpiredAt, time.Second)
 			},
 		},
 		{
-			name:          "DecryptError",
-			scope:         ScopeAccess,
-			userDiscordID: util.GenerateSnowflakeID().Int64(),
-			duration:      time.Minute,
-			checkVerify: func(t *testing.T, payload *Payload, err error) {
-				require.Error(t, err)
-				require.Empty(t, payload)
-			},
-		},
-		{
 			name:          "TokenExpired",
-			scope:         ScopeAccess,
-			userDiscordID: util.GenerateSnowflakeID().Int64(),
+			userDiscordID: userDiscordID,
 			duration:      -time.Minute,
 			checkVerify: func(t *testing.T, payload *Payload, err error) {
 				require.ErrorIs(t, err, ErrExpiredToken)
-				require.Empty(t, payload)
-			},
-		},
-		{
-			name:          "UnsupportedScope",
-			scope:         "unsupported",
-			userDiscordID: util.GenerateSnowflakeID().Int64(),
-			duration:      time.Minute,
-			checkVerify: func(t *testing.T, payload *Payload, err error) {
-				require.Error(t, err)
 				require.Empty(t, payload)
 			},
 		},
@@ -103,7 +77,7 @@ func TestPasetoTokenMaker(t *testing.T) {
 			maker, err := NewPasetoMaker(util.RandomString(32))
 			require.NoError(t, err)
 
-			accessToken, err := maker.CreateToken(tc.scope, accountDiscordID, tc.duration)
+			accessToken, _, err := maker.CreateToken(userDiscordID, tc.duration)
 			require.NoError(t, err)
 			require.NotEmpty(t, accessToken)
 
@@ -111,8 +85,4 @@ func TestPasetoTokenMaker(t *testing.T) {
 			tc.checkVerify(t, payload, err)
 		})
 	}
-}
-
-func TestTest(t *testing.T) {
-	t.Log(util.RandomString(32))
 }

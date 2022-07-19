54b63cd75db7577e9683bfb94d21d9d9f816fd78
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-19 23:46:12 +0200

    Renamed from util to utils

--- cmd/main.go
+++ cmd/main.go
@@ -9,16 +9,17 @@ import (
 	"github.com/BoggerByte/Sentinel-backend.git/pkg/middlewares/permissions"
 	"github.com/BoggerByte/Sentinel-backend.git/pkg/modules/token"
 	"github.com/BoggerByte/Sentinel-backend.git/pkg/services"
-	"github.com/BoggerByte/Sentinel-backend.git/pkg/util"
+	"github.com/BoggerByte/Sentinel-backend.git/pkg/utils"
 	"github.com/gin-contrib/cors"
 	_ "github.com/lib/pq"
 	"github.com/ravener/discord-oauth2"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/oauth2"
+	"time"
 )
 
 func main() {
-	config, err := util.LoadConfig()
+	config, err := utils.LoadConfig()
 	if err != nil {
 		logrus.Fatalf("Failed to initialize config: %v", err.Error())
 	}
@@ -55,7 +56,7 @@ func main() {
 	discordOauth2Service := services.NewDiscordOauth2Service(&oauth2.Config{
 		Endpoint:     discord.Endpoint,
 		Scopes:       []string{discord.ScopeIdentify, discord.ScopeEmail, discord.ScopeGuilds},
-		RedirectURL:  "http://localhost:8080/api/v1/oauth2/discord_callback",
+		RedirectURL:  "http://localhost:3000/oauth2/discord_callback",
 		ClientID:     config.DiscordClientID,
 		ClientSecret: config.DiscordClientSecret,
 	})
@@ -67,9 +68,18 @@ func main() {
 		GuildConfig: controllers.NewGuildConfigController(store),
 		Oauth2:      controllers.NewOauth2Controller(store, memStore, config, tokenMaker, discordOauth2Service),
 	}
-
 	middlewaresV1 := middlewares.Middlewares{
-		CORS: cors.Default(),
+		CORS: cors.New(cors.Config{
+			AllowAllOrigins:        true,
+			AllowMethods:           []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+			AllowHeaders:           []string{"Content-Type", "Origin", "Access-Control-Allow-Origin", "Authorization", "Accept", "Accept-Encoding"},
+			AllowCredentials:       true,
+			ExposeHeaders:          []string{"Content-Length"},
+			MaxAge:                 12 * time.Hour,
+			AllowBrowserExtensions: true,
+			AllowWebSockets:        true,
+			AllowFiles:             true,
+		}),
 		Auth: middlewares.NewAuthMiddleware(tokenMaker),
 		Permissions: middlewares.Permissions{
 			GuildConfig: permissions.NewGuildConfigPermissions(store),

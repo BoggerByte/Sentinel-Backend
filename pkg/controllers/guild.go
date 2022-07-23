ad695d70c6008db06b26ffec1b2c7c36843fb947
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-23 19:15:54 +0200

    New get user guild route; Created additional perms fields for guild object responses

--- pkg/controllers/guild.go
+++ pkg/controllers/guild.go
@@ -2,16 +2,28 @@ package controllers
 
 import (
 	"database/sql"
+	"errors"
 	db "github.com/BoggerByte/Sentinel-backend.git/pkg/db/sqlc"
 	"github.com/BoggerByte/Sentinel-backend.git/pkg/forms"
 	"github.com/BoggerByte/Sentinel-backend.git/pkg/middlewares"
 	"github.com/BoggerByte/Sentinel-backend.git/pkg/modules/token"
+	"github.com/BoggerByte/Sentinel-backend.git/pkg/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 type GuildController struct {
 	store db.Store
+}
+
+type ResponseGuild struct {
+	ID             int64  `json:"id"`
+	DiscordID      string `json:"discord_id"`
+	OwnerDiscordID string `json:"owner_discord_id"`
+	Name           string `json:"name"`
+	Icon           string `json:"icon"`
+	CanReadConfig  bool   `json:"can_read_config"`
+	CanEditConfig  bool   `json:"can_edit_config"`
 }
 
 func NewGuildController(store db.Store) *GuildController {
@@ -38,7 +50,40 @@ func (ctrl *GuildController) Get(c *gin.Context) {
 	c.JSON(http.StatusOK, guild)
 }
 
-func (ctrl *GuildController) GetAll(c *gin.Context) {
+func (ctrl *GuildController) GetUserOne(c *gin.Context) {
+	var uri forms.GetUserGuildURI
+	if err := c.ShouldBindUri(&uri); err != nil {
+		c.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	payload := c.MustGet(middlewares.AuthorizationPayloadKey).(*token.Payload)
+
+	guild, err := ctrl.store.GetUserGuild(c, db.GetUserGuildParams{
+		AccountDiscordID: payload.UserDiscordID,
+		GuildDiscordID:   uri.DiscordID,
+	})
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			c.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		c.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	c.JSON(http.StatusOK, ResponseGuild{
+		ID:             guild.ID,
+		DiscordID:      guild.DiscordID,
+		OwnerDiscordID: guild.OwnerDiscordID,
+		Name:           guild.Name,
+		Icon:           guild.Icon,
+		CanReadConfig:  utils.AnyOfPermissions(guild.Permissions, guild.ConfigRead),
+		CanEditConfig:  utils.AnyOfPermissions(guild.Permissions, guild.ConfigEdit),
+	})
+}
+
+func (ctrl *GuildController) GetUserAll(c *gin.Context) {
 	payload := c.MustGet(middlewares.AuthorizationPayloadKey).(*token.Payload)
 
 	guilds, err := ctrl.store.GetUserGuilds(c, payload.UserDiscordID)
@@ -47,5 +92,18 @@ func (ctrl *GuildController) GetAll(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, guilds)
+	var rGuilds []ResponseGuild
+	for _, guild := range guilds {
+		rGuilds = append(rGuilds, ResponseGuild{
+			ID:             guild.ID,
+			DiscordID:      guild.DiscordID,
+			OwnerDiscordID: guild.OwnerDiscordID,
+			Name:           guild.Name,
+			Icon:           guild.Icon,
+			CanReadConfig:  utils.AnyOfPermissions(guild.Permissions, guild.ConfigRead),
+			CanEditConfig:  utils.AnyOfPermissions(guild.Permissions, guild.ConfigEdit),
+		})
+	}
+
+	c.JSON(http.StatusOK, rGuilds)
 }
--- pkg/db/query/guild.sql
+++ pkg/db/query/guild.sql
@@ -2,14 +2,17 @@
 INSERT INTO guild (discord_id, name, icon, owner_discord_id)
 VALUES ($1, $2, $3, $4)
 ON CONFLICT (discord_id) DO UPDATE
-    SET name=$2,
-        icon=$3,
-        owner_discord_id=$4
+    SET name             = $2,
+        icon             = $3,
+        owner_discord_id = $4
 RETURNING *;
 
 -- name: GetGuild :one
-SELECT *
-FROM guild
+SELECT g.*,
+       (gc.json::json -> 'permissions' ->> 'read')::bigint AS config_read,
+       (gc.json::json -> 'permissions' ->> 'edit')::bigint AS config_edit
+FROM guild g
+         INNER JOIN guild_config gc ON gc.id = g.id
 WHERE discord_id = $1
 LIMIT 1;
 
@@ -18,13 +21,32 @@ DELETE
 FROM guild
 WHERE discord_id = $1;
 
--- name: GetUserGuilds :many
+-- name: GetUserGuild :one
 SELECT coalesce(g.id, 0),
-       ug.guild_discord_id AS discord_id,
+       ug.guild_discord_id                                 AS discord_id,
        ug.permissions,
        coalesce(g.owner_discord_id, '0'),
        coalesce(g.icon, '#'),
-       coalesce(g.name, '')
+       coalesce(g.name, ''),
+       (gc.json::json -> 'permissions' ->> 'read')::bigint AS config_read,
+       (gc.json::json -> 'permissions' ->> 'edit')::bigint AS config_edit
 FROM user_guild ug
          LEFT OUTER JOIN guild g ON g.discord_id = ug.guild_discord_id
+         INNER JOIN guild_config gc ON gc.id = g.id
+WHERE ug.account_discord_id = $1
+  AND ug.guild_discord_id   = $2
+LIMIT 1;
+
+-- name: GetUserGuilds :many
+SELECT coalesce(g.id, 0),
+       ug.guild_discord_id                                 AS discord_id,
+       ug.permissions,
+       coalesce(g.owner_discord_id, '0'),
+       coalesce(g.icon, '#'),
+       coalesce(g.name, ''),
+       (gc.json::json -> 'permissions' ->> 'read')::bigint AS config_read,
+       (gc.json::json -> 'permissions' ->> 'edit')::bigint AS config_edit
+FROM user_guild ug
+         LEFT OUTER JOIN guild g ON g.discord_id = ug.guild_discord_id
+         INNER JOIN guild_config gc ON gc.id = g.id
 WHERE ug.account_discord_id = $1;
